2934e1da9f18eb68ccf10adff7f1ade1d9dce15f
hansapradhan <pradhan.hansa@gmail.com>
2020-07-19 16:48:36 +0530

    Combining Reducers

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody,CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import CommentForm from './CommentFormComponent'
 
 
     function RenderDish({dish}) {
@@ -32,6 +33,7 @@ import { Link } from 'react-router-dom';
                 <div className="col-12 col-md-5 m-1">
                     <h4>Comments</h4>
                     <ul className="list-unstyled">{commentList}</ul>
+                    <CommentForm />
                 </div>
             )
         } else {
